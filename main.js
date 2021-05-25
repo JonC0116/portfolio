77d0143addaab45b1ccd9054295d63ef4fd27e19
Jeonghwan Choi <jonchoi0116@gmail.com>
2021-05-24 22:14:14 -0500

    navbar menu toggle

--- main.js
+++ main.js
@@ -19,10 +19,15 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
 
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
 
 // Handle jumping to the Contact Me section when clicking the 'contact me' button
 const contactMeBtn = document.querySelector('.home__contact');
